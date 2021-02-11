2461f09ef0119f50ee4e5b45b88c791ee8e7f0df
emre <emrecanocak@windowslive.com>
2021-02-11 08:41:12 +0300

    makelib now makes another copy without using debuger

--- builder/makelib.bash
+++ builder/makelib.bash
@@ -6,28 +6,34 @@
 # example: simply run it in directory that has a .asm file
 #	./makelib.bash
 
-lib_path="/home/eco/assembly/lib"
+lib_path="../lib"
+suffix="_dbg"
 file=$(ls | grep ".*\.\(asm\)")
+name="${file%.*}"
 if [ "$file" == "" ]
 then
 	echo "there is no file with ext. '.asm'"
 	exit 127
 fi
 
-name="${file%.*}"
-echo "> building $file"
-exit=$((nasm -f elf32 -g "$name".asm) 2>&1 )
+echo "> building $file with debug"
+exit=$((nasm -f elf32 -g "$name".asm -o "$name$suffix".o) 2>&1 )
 if [ "$exit" != "" ]
 then
 	echo "$exit"
 	exit 0
 fi
 
-exit=$(mv "$name".o "$lib_path")
+echo "> building $file without debug"
+exit=$((nasm -f elf32 "$name".asm -o "$name".o) 2>&1 )
 if [ "$exit" != "" ]
 then
 	echo "$exit"
 	exit 0
 fi
 
-echo "created as $lib_path/$name.o"
+
+$(mv "$name".o "$lib_path")
+$(mv "$name$suffix".o "$lib_path")
+echo created as $lib_path/$name.o
+echo created as $lib_path/"$name$suffix".o
